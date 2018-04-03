93d3dd17b975e2ca95647acb864afa68925d314e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-04-04 02:41:26 +0900

    Warning about legacy string ref API usage

--- public/components/App.js
+++ public/components/App.js
@@ -15,10 +15,50 @@ class UnsafeComponent extends Component {
   }
 }
 
+class LegacyRef extends Component {
+  handleClick = () => {
+    const name = this.refs.name.value;
+    console.log('LegacyRef', name);
+  };
+
+  render() {
+    return (
+      <div>
+        <input ref="name" />
+        <button onClick={this.handleClick}>click me</button>
+      </div>
+    );
+  }
+}
+
+class NewRef extends Component {
+  constructor(props) {
+    super(props);
+
+    this.nameRef = React.createRef();
+  }
+
+  handleClick = () => {
+    const name = this.nameRef.current.value;
+    console.log('NewRef', name);
+  };
+
+  render() {
+    return (
+      <div>
+        <input ref={this.nameRef} />
+        <button onClick={this.handleClick}>click me</button>
+      </div>
+    );
+  }
+}
+
 export default function App() {
   return (
     <StrictMode>
       <UnsafeComponent />
+      <LegacyRef />
+      <NewRef />
     </StrictMode>
   );
 }
